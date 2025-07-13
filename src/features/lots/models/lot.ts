180e582249bb032263a654c599d84a0f24fbcb35
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-13 02:16:38 +0000

    Feat(lot): add fields to lot model

--- src/features/lots/models/lot.ts
+++ src/features/lots/models/lot.ts
@@ -1,3 +1,12 @@
-export interface Lot{
+import type {Boisson} from "../../boissons/models/boisson.ts";
 
+export interface Lot{
+    id?: number;
+    numeroLot: string;
+    quantiteInitiale: number;
+    quantiteActuelle: number;
+    dateEntree: string;
+    datePeremption: string;
+    boisson: Boisson;
+    vendable: boolean;
 }
