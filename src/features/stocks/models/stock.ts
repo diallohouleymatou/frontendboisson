009252b6405c049ab4853e3668d1090ce1eb2900
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-13 02:20:57 +0000

    Refactor: Update Stock model

--- src/features/stocks/models/stock.ts
+++ src/features/stocks/models/stock.ts
@@ -1,1 +1,9 @@
-export interface Stock{}
+import type {Boisson} from "../../boissons/models/boisson.ts";
+
+export interface Stock{
+    id?: number;
+    boisson: Boisson;
+    quantiteTotale: number;
+    createdAt?: string;
+    updatedAt?: string;
+}
