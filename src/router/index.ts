4992f4b67b175130f64019543094b447d1cb3cf4
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-14 02:57:31 +0000

    Refactor: Update router index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,19 @@
 import BoissonPage from "../pages/BoissonPage.vue";
 import {createRouter, createWebHistory} from "vue-router";
+import TableauBordPage from "../pages/TableauBordPage.vue";
+import MouvementPage from "../pages/MouvementPage.vue";
+import LotPage from "../pages/LotPage.vue";
+import StockPage from "../pages/StockPage.vue";
+import UtilisateurPage from "../pages/UtilisateurPage.vue";
 
 const routes = [
     {path:'/',redirect:'/boisson'},
     {path:'/boisson',component:BoissonPage},
+    {path:'/dashboard',component:TableauBordPage},
+    {path:'/mouvement',component:MouvementPage},
+    {path:'/lot',component:LotPage},
+    {path:'/stock',component:StockPage},
+    {path:'/utilisateur',component:UtilisateurPage},
 ]
 const router = createRouter({
     history:createWebHistory(),
