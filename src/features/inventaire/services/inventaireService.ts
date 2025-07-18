c980eb58411de996ac64be51d7a92438d5e041db
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-18 16:57:27 +0000

    Feat: Add inventaireService

--- src/features/inventaire/services/inventaireService.ts
+++ src/features/inventaire/services/inventaireService.ts
@@ -1,5 +1,6 @@
 import type { CreateLotRequest } from '../models/createLotRequest';
 import type { LigneOperation } from '../models/ligneOperation';
+import type { Lot } from '../models/lot';
 import { api } from '../../../api/api';
 
 export const inventaireService = {
@@ -7,5 +8,30 @@ export const inventaireService = {
     const response = await api.post('/inventaire/entree', request);
     return response.data;
   },
+  async getAllLots(): Promise<Lot[]> {
+    const response = await api.get('/inventaire/lots');
+    return response.data;
+  },
+  async getAllMouvements() {
+    const response = await api.get('/inventaire/mouvements');
+    console.log(response.data);
+    return response.data;
+  },
+  async getAllLigneOperations() {
+    const response = await api.get('/inventaire/ligne-operations');
+    console.log(response.data);
+    return response.data;
+  },
+  async createMouvementSortie(request: any) {
+    const response = await api.post('/inventaire/sortie', request);
+    return response.data;
+  },
+  async createMouvementAjustement(request: any) {
+    const response = await api.post('/inventaire/ajustement', request);
+    return response.data;
+  },
+  async createMouvementEntree(request: any) {
+    const response = await api.post('/inventaire/entree', request);
+    return response.data;
+  },
 };
-
