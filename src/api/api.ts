2d63e4e23d8d7bb9efd2b18f4bf0c4ccc7eaad43
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-18 19:40:23 +0000

    feat: increase api timeout

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios, {type AxiosInstance} from "axios";
 const api_baseUrl="http://localhost:8080/examgestionboisson_war_exploded/api";
 const api :AxiosInstance=axios.create({
     baseURL:api_baseUrl,
-    timeout:5000,
+    timeout:30000,
     headers:{'Content-Type':'application/json'}
 })
 
