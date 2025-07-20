8078ccfba12b66a9bfc104a4b53ad49a120c2d68
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-20 22:48:16 +0000

    Feat: Add StockAlertDto and DashboardStatisticsDto interfaces

--- src/features/stats/models/stat.ts
+++ src/features/stats/models/stat.ts
@@ -1,1 +1,18 @@
 export interface Stat{}
+
+export interface StockAlertDto {
+  lotId: number;
+  boisson: string;
+  quantite: number;
+  seuil: number;
+}
+
+export interface DashboardStatisticsDto {
+  totalBeverages: number;
+  totalStock: number;
+  lowStockAlerts: number;
+  totalMovements: number;
+  totalUsers: number;
+  totalValue: number;
+  stockAlerts?: StockAlertDto[];
+}
