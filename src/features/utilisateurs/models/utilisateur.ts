69c902f185d864abcb128d1bcb3c0654ec70e61c
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-20 16:57:17 +0000

    Refactor: Update utilisateur model

--- src/features/utilisateurs/models/utilisateur.ts
+++ src/features/utilisateurs/models/utilisateur.ts
@@ -1,11 +1,14 @@
 import type {Role} from "./role.ts";
 
-export interface Utilisateur{
+export interface Utilisateur {
     id: number;
+    firstName: string;
+    lastName: string;
     email: string;
     role: Role;
-    motDePasse: string;
+    motDePasse?: string;
     isActive: boolean;
+    firstLogin: boolean;
     createdAt?: string;
     updatedAt?: string;
 }
