1e6ba16002fcf38d7d7b78e7009107e240ac6667
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-20 22:48:26 +0000

    Feat: Implement getDashboardStats in StatService

--- src/features/stats/services/statService.ts
+++ src/features/stats/services/statService.ts
@@ -1,1 +1,10 @@
-export class StatService {}
+
+import type { DashboardStatisticsDto } from '../models/stat';
+import { api } from '../../../api/api';
+export class StatService {
+  static async getDashboardStats(): Promise<DashboardStatisticsDto> {
+    const response = await api.get('/statistics/dashboard');
+    return response.data;
+  }
+}
+
