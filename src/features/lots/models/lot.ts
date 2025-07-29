67deeda76a17c6239a2e5d6d2e2a8a783407c6b9
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-29 02:03:05 +0000

    Reglage: ajoute propriété fournisseur à l'interface Lot pour liaison directe avec Fournisseur

--- src/features/lots/models/lot.ts
+++ src/features/lots/models/lot.ts
@@ -1,7 +1,9 @@
 import type {Boisson} from "../../boissons/models/boisson.ts";
+import type {Fournisseur} from "../../fournisseur/models/fournisseur.ts";
 
 export interface Lot{
     id?: number;
+    fournisseur: Fournisseur;
     numeroLot: string;
     quantiteInitiale: number;
     quantiteActuelle: number;
