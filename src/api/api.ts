7ab9c005aea6814d75871bbe937e90fbc27d7937
houleymatou-diallo <dhouleymatou150@gmail.com>
2025-07-30 17:24:20 +0000

    Config: utilise la variable d'environnement VITE_API_URL pour l'URL de base de l'API et ajoute le fichier .env

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import axios, {type AxiosInstance} from "axios";
-const api_baseUrl="http://localhost:8080/examgestionboisson_war_exploded/api/";
+const api_baseUrl = import.meta.env.VITE_API_URL + "examgestionboisson_war_exploded/api/";
 const api :AxiosInstance=axios.create({
     baseURL:api_baseUrl,
     timeout:120000, // timeout augmenté à 2 minutes
